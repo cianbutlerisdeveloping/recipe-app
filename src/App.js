acc7c9515fff2dfb3dd9965f807bcbc63eac3db6
Cian Butler <cianbutlerisdeveloping@gmail.com>
2020-01-09 22:01:08 +0000

    set-up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "c194ec8d";
+  const APP_KEY = "a7203f5defb7a280b3a0389f353eb02d";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
